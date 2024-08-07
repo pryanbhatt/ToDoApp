3a5daa10113f03b790178e38c9b8258d02d74533
Bhattacharjee <pbhatt208@apac.comcast.com>
2024-08-07 22:20:39 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,46 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert('You must type a task name');
+    }
+    
+    else{
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+        
+        listContainer.append(li);
+      
+    }
+    
+    inputBox.value='';
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === 'LI'){
+       e.target.classList.toggle('checked');
+        saveData()
+    }
+    
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+    }
+    
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTasks(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTasks();
